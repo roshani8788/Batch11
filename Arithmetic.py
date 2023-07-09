60a1a1512c14aa40c286606a90ac97f332ce1f4f
roshani8788 <roshanikatekar99@gmail.com>
2023-07-09 14:15:03 +0530

    Added file for Multiplication

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a + b
 
+def mul(a,b):
+    return a * b
 
 sum = add(10, 20)
+mulit = mul(2,3)
 print(f"Addition of two numbers:{sum}")
+print(f"Multiplication of numbers:{mulit}")
